8e7049489e1ce0dd859afdacd783e3f6402fb48b
silvestrijonathan <silvestrijonathan@gmail.com>
2019-10-06 16:24:49 -0400

    Caching the responses of previously input requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./App.css";
 
-const numbersDict = {};
+const createLocalTimeStamp = () => {
+  const now = new Date();
+  const offsetMs = now.getTimezoneOffset() * 60 * 1000;
+  const dateLocal = new Date(now.getTime() - offsetMs);
+  return dateLocal
+    .toISOString()
+    .slice(0, 19)
+    .replace(/-/g, "/")
+    .replace("T", " ");
+};
 
 const sumOfSquares = number => {
   let sum = 0;
@@ -21,25 +30,30 @@ const squareOfSum = number => {
   return sum ** 2;
 };
 
-const createApiResponse = number => {
-  if (numbersDict[number]) {
-    numbersDict[number].last_datetime = numbersDict[number].datetime;
-    numbersDict[number].datetime = new Date();
-    numbersDict[number].occurrences++;
-    return JSON.stringify(numbersDict[number]);
-  }
+const memoizedApiResponse = () => {
+  const cache = {};
 
-  const response = {
-    datetime: new Date(),
-    value: squareOfSum(number) - sumOfSquares(number),
-    number,
-    occurrences: 1,
-    last_datetime: null
+  return number => {
+    if (cache[number]) {
+      cache[number].last_datetime = cache[number].datetime;
+      cache[number].datetime = createLocalTimeStamp();
+      cache[number].occurrences++;
+      return JSON.stringify(cache[number]);
+    } else {
+      const response = {
+        datetime: createLocalTimeStamp(),
+        value: squareOfSum(number) - sumOfSquares(number),
+        number,
+        occurrences: 1,
+        last_datetime: null
+      };
+      cache[number] = response;
+      return JSON.stringify(response);
+    }
   };
-
-  numbersDict[number] = response;
-  return JSON.stringify(response);
 };
+
+const createApiResponse = memoizedApiResponse();
 
 const mockApi = number =>
   new Promise(resolve => {
@@ -51,7 +65,7 @@ function App() {
   const [submittedValue, setSubmittedValue] = React.useState("");
   const [error, setError] = React.useState("");
   const [loading, setLoading] = React.useState(false);
-  const [response, setResponse] = React.useState([]);
+  const [responses, setResponses] = React.useState([]);
 
   React.useEffect(() => {
     if (!submittedValue) return; // Guarding against my falsy default
@@ -61,7 +75,9 @@ function App() {
       !didCancel && setLoading(true);
       const apiResponse = await mockApi(submittedValue);
       !didCancel &&
-        setResponse(prevResponse => [...prevResponse, JSON.parse(apiResponse)]);
+        setResponses(prevResponses =>
+          [...prevResponses, JSON.parse(apiResponse)].reverse()
+        ); // Ensure most recent requests show first.
       !didCancel && setLoading(false);
       setSubmittedValue("");
     };
@@ -101,19 +117,36 @@ function App() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <p>{error}</p>}
-      <label htmlFor="number">
-        Input a number:{" "}
-        <input
-          name="number"
-          id="number"
-          value={value}
-          onChange={handleChange}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        {error && <p>{error}</p>}
+        <label htmlFor="number">
+          Input a number:{" "}
+          <input
+            name="number"
+            id="number"
+            value={value}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {!!responses.length && <h2>Previous API Responses</h2>}
+      {responses.map((response, index) => (
+        <div>
+          <h3>
+            Request for {response.number} at {response.datetime}
+          </h3>
+          <ul key={index}>
+            <li>Datetime: {response.datetime}</li>
+            <li>Number: {response.number}</li>
+            <li>Value: {response.value}</li>
+            <li>Occurrences: {response.occurrences}</li>
+            <li>Last Requested: {response.last_datetime || "never"}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
   );
 }
 
